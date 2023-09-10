9983e289f4b5555282376cd25f25956a85815068
Skyino7 <christambassador12@gmail.com>
2023-09-10 18:31:43 +0000

    Updated

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,16 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ *
  */
 typedef struct listint_s
 {
@@ -22,6 +19,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-int is_palindrome(listint_t **head);
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,5 +8,19 @@
 
 int is_palindrome(listint_t **head)
 {
+    listint_t *slow;
+    listint_t *fast;
+
+    slow = head;
+    fast = head;
+
+    if (head == NULL && *head == NULL)
+        return (NULL);
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
 
 }
