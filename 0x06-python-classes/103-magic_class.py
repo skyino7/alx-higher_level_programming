6548152b32e4ec3a41551b35c702bca5e9b72db8
Skyino7 <christambassador12@gmail.com>
2023-09-27 05:55:46 +0000

    updated

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,28 +7,30 @@ import math
 class MagicClass:
     """
     Magic Class
+
     Attributes:
         radius (int): integer
     """
 
     def __init__(self, radius):
         """
+        init
+
         Args:
             radius (int): integer
-            Raises: Typeerror: not an int
+
+        Raises:
+            Typeerror: not an int
         """
+
         if not isinstance(radius, (int, float)):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """
-            float: area
-        """
+        """float: area"""
         return 2 ** self.__radius * math.pi
 
     def circumference(self):
-        """
-            float: circumference
-        """
+        """float: circumference"""
         return 2 * math.pi * self.__radius
