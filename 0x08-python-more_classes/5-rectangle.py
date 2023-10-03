1666bce590ef66cf94ad4b0364a420859c3f8a26
Skyino7 <christambassador12@gmail.com>
2023-10-03 12:47:49 +0000

    Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -104,3 +104,6 @@ class Rectangle:
     def __repr__(self):
         """ return a string representation of the rectangle """
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
