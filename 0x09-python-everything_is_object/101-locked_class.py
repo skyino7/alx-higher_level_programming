a765152641fa0980ee430a96fe9287bd205a52f9
Skyino7 <christambassador12@gmail.com>
2023-10-03 20:42:22 +0000

    requirement error fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Module
+"""
+
 
 class LockedClass:
+    """
+    Write a class LockedClass with no class or object attribute,
+    that prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called first_name.
+    """
+
     def __setattr__(self, name, value):
         if name != 'first_name':
             raise AttributeError(
