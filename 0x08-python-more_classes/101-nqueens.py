8921fa26d301398e534e83d5eea679c4a46edd83
Skyino7 <christambassador12@gmail.com>
2023-10-03 09:44:43 +0000

    queen chess

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    for i in range(col):
+        if board[row][i] == 1 or board[i][col] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, N):
+    if col == N:
+        print_solution(board, N)
+        return
+
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, N)
+            board[i][col] = 0
+
+
+def print_solution(board, N):
+    solution = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for i in range(N)] for j in range(N)]
+    solve_nqueens_util(board, 0, N)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    nqueens(sys.argv[1])
