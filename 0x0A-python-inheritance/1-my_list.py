ff7ee901ba06afbb0c41e2dd37f4ca3b38512e42
Skyino7 <christambassador12@gmail.com>
2023-10-09 20:10:00 +0000

    updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
-"""_summary_
+"""
+Module
 """
 
 
 class MyList(list):
+    """
+    class MyList that inherits from list
+
+    Args:
+        list (list): inherits from list
+    """
+
     def print_sorted(self):
+        """
+        prints sorted list
+        """
         sorted_list = sorted(self)
         print(sorted_list)
