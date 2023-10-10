d10201700c0c294870113e6e3ed0daf57c4fe947
Skyino7 <christambassador12@gmail.com>
2023-10-10 18:22:13 +0000

    Updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,11 +9,6 @@ def read_file(filename=""):
     Args:
         filename (str): reads a text file (UTF8).
     """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            text = file.read()
-            print(text)
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+        print(text)
