2c384abbf6ffd20701aa448f7e0713526603f4f3
Skyino7 <christambassador12@gmail.com>
2023-10-10 22:21:53 +0000

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Student:
+    """
+    Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+
+        Args:
+            first_name (str): Student First Name
+            last_name (str): Student Last Name
+            age (int): Student Age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): A list of strings
+
+        Returns:
+            dict: Dictionary represents the student instance
+        """
+        if attrs is None:
+
+            student_dict = {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+        else:
+            student_dict = {attr: getattr(self, attr)
+                            for attr in attrs if hasattr(self, attr)}
+
+        return student_dict
+
+    def reload_from_json(self, json):
+        """
+        Args:
+            json (dict): dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
