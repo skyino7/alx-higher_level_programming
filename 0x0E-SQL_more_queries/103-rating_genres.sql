2ab126db375e0e2c1990a9bfdaadaf81d3cc7970
Skyino7 <christambassador12@gmail.com>
2023-11-16 19:16:07 +0000

    updated

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_genres.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.id, tv_genres.name
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
