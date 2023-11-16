3780db6768eb2ed7e248a96cf2230684fe295d81
Skyino7 <christambassador12@gmail.com>
2023-11-16 18:44:24 +0000

    updated

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,11 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_shows.id
-    FROM tv_genres
-    JOIN tv_show_genres ON tv_genres.id = tv_genres.genre_id
-    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
     WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title ASC;
