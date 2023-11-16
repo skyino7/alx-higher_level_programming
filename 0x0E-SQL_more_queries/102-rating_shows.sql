73c4cec22bcc51798e8c11dec9178d1cbbd56ef3
Skyino7 <christambassador12@gmail.com>
2023-11-16 19:07:16 +0000

    fixed

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT tv_shows.title, SUM(tv_shows_rate.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
-LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_shows.id, tv_shows.title
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
