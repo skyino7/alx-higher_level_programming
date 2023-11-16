416f86c965a272ee6da331b7f0f083b7812cd71e
Skyino7 <christambassador12@gmail.com>
2023-11-16 18:56:29 +0000

    fixed

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,7 +5,7 @@ FROM tv_shows
 WHERE tv_shows.title NOT IN (
     SELECT tv_shows.title
     FROM tv_shows
-    LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
