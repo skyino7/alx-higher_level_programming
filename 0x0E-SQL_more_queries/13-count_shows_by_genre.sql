56b55553de8210aba86b89c8db27da254489ee5b
Skyino7 <christambassador12@gmail.com>
2023-11-16 17:54:24 +0000

    UPDATED

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
 GROUP BY tv_genres.genre
 ORDER BY number_of_shows DESC;
