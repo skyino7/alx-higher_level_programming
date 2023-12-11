acfddc02a5dc5ccedcce2eebdf5a0a5793641d3f
Seth Ketsi <christambassador12@gmail.com>
2023-12-11 21:19:33 +0000

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv.slice(2);
+const arg1 = parseInt(arg[0]);
+let i = 0;
+if (isNaN(arg1)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < arg1; i++) {
+    console.log('X'.repeat(arg1));
+  }
+}
