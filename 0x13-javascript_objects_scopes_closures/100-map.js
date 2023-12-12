c1e566182a024cfeae4eeed88588cd62c2793b95
Seth Ketsi <christambassador12@gmail.com>
2023-12-12 19:26:51 +0000

    Write a script that imports an array and computes a new array.

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a script that imports an array and computes a new array.
+
+const list = require('./100-data').list;
+console.log(list);
+const newList = list.map((x, index) => x * index);
+console.log(newList);
