58dee59e079fc06026414dd3a8bfcb8ccf3e5677
Seth Ketsi <christambassador12@gmail.com>
2023-12-15 14:36:05 +0000

    Function that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module Docs"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Function that takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches the argument."""
+
+    dbHost = "localhost"
+    dbPort = 3306
+
+    db = MySQLdb.connect(host=dbHost, port=dbPort, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
+    arg = query.format(argv[4])
+
+    cursor.execute(arg)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
