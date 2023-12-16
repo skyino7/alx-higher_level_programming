901a0c28aa2f08b92dd4fa9c8246f4e48d913bb8
Seth Ketsi <christambassador12@gmail.com>
2023-12-16 09:34:04 +0000

    Function that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""Module Docs"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def main():
+    """Function that prints the State object with the name passed as argument
+    from the database hbtn_0e_6_usa"""
+
+    dbUser = argv[1]
+    dbPass = argv[2]
+    dbName = argv[3]
+    stateName = argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(dbUser, dbPass, dbName), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    qs = (session.query(State).filter(State.name == stateName).order_by(State.id).all())
+
+    if qs:
+        print(qs[0].id)
+    else:
+        print("Not found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
