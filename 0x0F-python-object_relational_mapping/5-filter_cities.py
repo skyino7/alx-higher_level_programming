4815980ff4a9e6a65a8ca45573cd88d9943ca91c
Seth Ketsi <christambassador12@gmail.com>
2023-12-16 00:12:15 +0000

    updated

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,8 +17,9 @@ def main():
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
 
-    query = "SELECT cities.name FROM cities INNER JOIN states ON \
-    cities.state_id = states.id WHERE states.name LIKE %s ORDER BY cities.id ASC"
+    query = "SELECT cities.name FROM cities INNER JOIN states ON " \
+        "cities.state_id = states.id WHERE states.name LIKE %s ORDER BY " \
+        "cities.id ASC"
     arg = (argv[4],)
 
     cursor.execute(query, arg)
