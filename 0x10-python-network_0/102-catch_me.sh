42a4e26cab7bd39624bc2537a9d4026d340d5412
Seth Ketsi <christambassador12@gmail.com>
2024-01-05 23:54:17 +0000

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to server and that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin:School"
