eabc2b392dc22cda5ef628138239154f3556e78d
Seth Ketsi <christambassador12@gmail.com>
2024-01-05 19:09:31 +0000

    pycodestyle fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ Module that finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
     """ Function that finds a peak in a list of unsorted integers """
     if not list_of_integers:
         return None
 
-    l, h = 0, len(list_of_integers) - 1
+    lw, hi = 0, len(list_of_integers) - 1
 
-    while l < h:
-        mid = (l + h) // 2
+    while lw < hi:
+        mid = (lw + hi) // 2
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            h = mid
+            hi = mid
         else:
-            l = mid + 1
+            lw = mid + 1
 
-    return list_of_integers[l]
+    return list_of_integers[lw]
