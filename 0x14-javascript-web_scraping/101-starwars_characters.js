e82ec460d71d8ce9915273fa28aa3ab78380387c
Seth Ketsi <christambassador12@gmail.com>
2024-01-30 18:07:08 +0000

    updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, (err, res, body) => {
-  if (err) { console.log(err); } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (err, res, body) => {
-        if (err) { console.log(err); } else {
-          console.log(JSON.parse(body).name);
+
+request(url, { json: true }, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+        console.error(err || res.statusCode);
+    } else {
+        for (const characterUrl of body.characters) {
+            request(characterUrl, { json: true }, (charErr, charRes, charBody) => {
+                if (charErr || charRes.statusCode !== 200) {
+                    console.error(charErr || charRes.statusCode);
+                } else {
+                    console.log(`${charBody.name}`);
+                }
+            });
         }
-      });
     }
-  }
 });
